b0be8f42bb260866b4083ed0da30b4e9f561c7fd
Stuffy <77872467+stuffyerface@users.noreply.github.com>
2024-04-16 17:51:41 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+playerStartTime = {};
+playerTimeSpent = {};
+active = false
+
+
+register('chat', (rank,player) => {
+    active = true;
+    playerStartTime[player] = Date.now();
+}).setCriteria("⚔${rank}${player} warped to your instance")
+
+register('chat', (player, location) => {
+    let now = Date.now()
+    for (let key in playerStartTime){
+        let timeSpent = now - playerStartTime[key];
+        playerTimeSpent[player] = timeSpent/1000 + "&es";
+    }
+    // if active and auto warp, warp here
+}).setCriteria(" » ${player} is traveling to ${location} FOLLOW")
+
+register('chat', () => {
+    ChatLib.chat("&b▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n&eTracked Shaft Times:")
+    let now = Date.now()
+    for (let key in playerStartTime){
+        if (!playerTimeSpent[key]){
+            playerTimeSpent[key] = (now - playerStartTime[key])/1000 + "&es &cDNF";
+        }
+    }
+    for (let key in playerTimeSpent){
+        let timeSpent = playerTimeSpent[key]
+        ChatLib.chat(" &3" + key + ": &f" + timeSpent)
+    }
+    ChatLib.chat("&b▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+    reset()
+}).setCriteria("The mineshaft entrance has caved in... it doesn't look like anyone else will be able to get in here.")
+
+register('worldLoad', () =>{
+    reset()
+})
+
+function reset() {
+    active = false;
+    playerStartTime = {};
+    playerTimeSpent = {};
+}
