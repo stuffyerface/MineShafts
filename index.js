26c7e1d244ed5be2327139fd867b269855cb04a9
Stuffy <77872467+stuffyerface@users.noreply.github.com>
2024-04-18 15:10:44 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ let active = false
 
 
 register('chat', (player) => {
-    player = player.split(" ")[-1]
+    player = player.split(" ").slice(-1)
     active = true;
+    //console.log("'" + player + "' warped in.")
     playerStartTime[player] = Date.now();
 }).setCriteria("⚔${player} warped to your instance")
 
 register('chat', (player, location) => {
+    if(!active || !playerStartTime[player]) {return};
+    //console.log("'" + player + "' warped out.")
     let now = Date.now()
-    for (let key in playerStartTime){
-        let timeSpent = now - playerStartTime[key];
-        playerTimeSpent[player] = timeSpent/1000 + "&es";
-    }
+    let timeSpent = now - playerStartTime[player];
+    playerTimeSpent[player] = timeSpent/1000 + "&es";
     // if active and auto warp, warp here
 }).setCriteria(" » ${player} is traveling to ${location} FOLLOW")
 
@@ -22,14 +23,23 @@ register('chat', () => {
     if(active){
         ChatLib.chat("&b▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n&eTracked Shaft Times:")
         let now = Date.now()
-        for (let key in playerStartTime){
-            if (!playerTimeSpent[key]){
-                playerTimeSpent[key] = (now - playerStartTime[key])/1000 + "&es &cDNF";
-            }
-        }
         for (let key in playerTimeSpent){
             let timeSpent = playerTimeSpent[key]
-            ChatLib.chat(" &3" + key + ": &f" + timeSpent)
+            let color = "f"
+            if(timeSpent > 40000){
+                color = "c"
+            }
+            ChatLib.chat(" &3" + key + ": &" + color + timeSpent)
+        }
+        for (let key in playerStartTime){
+            if (!playerTimeSpent[key]){
+                let color = "f"
+                    if(timeSpent > 40000){
+                color = "c"
+                }
+                let timeSpent = (now - playerStartTime[key])/1000 + "&es &cDNF";
+                ChatLib.chat(" &3" + key + ": &" + color + timeSpent)
+            }
         }
         ChatLib.chat("&b▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
         reset()
